61830dac363989bcc6d2f7e077ad005f4363f034
Gajendra Bisht <gajendrabisht@gajendras-mbp.lan>
2018-07-31 14:46:02 +0100

    Seconds test passing

--- src/main/java/com/hive/BerlinClock.java
+++ src/main/java/com/hive/BerlinClock.java
@@ -1,9 +1,16 @@
 package com.hive;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class BerlinClock {
+
+    public static final String YELLOW = "Y";
+    public static final String OFF = "0";
+    public static final String NEW_LINE = "\n";
 
     private String time;
 
@@ -12,8 +19,11 @@ public class BerlinClock {
         this.time = convertToBerlinClockTime(localTime);
     }
 
-    private String convertToBerlinClockTime(LocalTime locaTime) {
-        return null;
+    private String convertToBerlinClockTime(LocalTime localTime) {
+        String seconds = (localTime.getSecond() % 2 == 0) ? YELLOW : OFF;
+        String hours = "0000\n0000";
+        String minutes = "00000000000\n0000";
+        return StringUtils.join(Arrays.asList(seconds, hours, minutes), NEW_LINE);
     }
 
     public String display() {
