7b82c380547375da417db9b5676943e5895d3127
Gajendra Bisht <gajendrabisht@gmail.com>
2018-08-01 10:07:35 +0100

    Minutes test passing

--- src/main/java/com/hive/BerlinClock.java
+++ src/main/java/com/hive/BerlinClock.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,6 +15,7 @@ public class BerlinClock {
     public static final String YELLOW = "Y";
     public static final String RED = "R";
     public static final String NEW_LINE = "\n";
+    public static final String BLANK = "";
 
     private String time;
 
@@ -22,24 +24,37 @@ public class BerlinClock {
         this.time = convertToBerlinClockTime(localTime);
     }
 
+    public String displayTime() {
+        return this.time;
+    }
+
     private String convertToBerlinClockTime(LocalTime localTime) {
         String seconds = (localTime.getSecond() % 2 == 0) ? YELLOW : OFF;
         String hoursFirstRow = turnOnRowOfLamps(4, localTime.getHour() / 5, RED);
         String hoursSecondRow = turnOnRowOfLamps(4, localTime.getHour() % 5, RED);
-        String minutes = "00000000000\n0000";
-        return StringUtils.join(Arrays.asList(seconds, hoursFirstRow, hoursSecondRow, minutes), NEW_LINE);
+        String minutesFirstRow = flipToRedAfterConsecutiveYellows(turnOnRowOfLamps(11, localTime.getMinute() / 5, YELLOW));
+        String minutesSecondRow = turnOnRowOfLamps(4, localTime.getMinute() % 5, YELLOW);
+        return StringUtils.join(Arrays.asList(seconds, hoursFirstRow, hoursSecondRow, minutesFirstRow, minutesSecondRow), NEW_LINE);
     }
 
-    public String turnOnRowOfLamps(int numberOfLamps, int LampsTurnedOn, String colour) {
+    private String turnOnRowOfLamps(int numberOfLamps, int LampsTurnedOn, String colour) {
         return StringUtils.rightPad(turnOnLamps(LampsTurnedOn, colour), numberOfLamps, OFF);
     }
 
-    public String turnOnLamps(int totalLampsTurnedOn, String colour) {
-        return StringUtils.join(IntStream.range(0, totalLampsTurnedOn).mapToObj(i -> colour).collect(Collectors.toList()), "");
+    private String turnOnLamps(int totalLampsTurnedOn, String colour) {
+        return combine(IntStream.range(0, totalLampsTurnedOn).mapToObj(i -> colour).collect(Collectors.toList()));
     }
 
-    public String display() {
-        return this.time;
+    private String flipToRedAfterConsecutiveYellows(String time) {
+        return StringUtils.replaceAll(time, combine(YELLOW, YELLOW, YELLOW), combine(YELLOW, YELLOW, RED));
+    }
+
+    private String combine(String... strings) {
+        return StringUtils.join(strings, BLANK);
+    }
+
+    private String combine(List<String> strings) {
+        return StringUtils.join(strings, BLANK);
     }
 
 }
--- src/test/java/com/hive/BerlinClockTest.java
+++ src/test/java/com/hive/BerlinClockTest.java
@@ -9,22 +9,33 @@ public class BerlinClockTest {
 
     @Test
     public void shouldDisplaySecondsEven() {
-        assertThat(new BerlinClock("00:00:02").display(), is("Y\n0000\n0000\n00000000000\n0000"));
+        assertThat(new BerlinClock("00:00:02").displayTime(), is("Y\n0000\n0000\n00000000000\n0000"));
     }
 
     @Test
     public void shouldDisplaySecondsOdd() {
-        assertThat(new BerlinClock("00:00:01").display(), is("0\n0000\n0000\n00000000000\n0000"));
+        assertThat(new BerlinClock("00:00:01").displayTime(), is("0\n0000\n0000\n00000000000\n0000"));
     }
 
     @Test
     public void shouldDisplayHoursEven() {
-        assertThat(new BerlinClock("10:00:01").display(), is("0\nRR00\n0000\n00000000000\n0000"));
+        assertThat(new BerlinClock("10:00:01").displayTime(), is("0\nRR00\n0000\n00000000000\n0000"));
     }
 
     @Test
     public void shouldDisplayHoursOdd() {
-        assertThat(new BerlinClock("11:00:01").display(), is("0\nRR00\nR000\n00000000000\n0000"));
+        assertThat(new BerlinClock("11:00:01").displayTime(), is("0\nRR00\nR000\n00000000000\n0000"));
     }
+
+    @Test
+    public void shouldDisplayMinutesEven() {
+        assertThat(new BerlinClock("00:20:01").displayTime(), is("0\n0000\n0000\nYYRY0000000\n0000"));
+    }
+
+    @Test
+    public void shouldDisplayMinutesOdd() {
+        assertThat(new BerlinClock("00:21:01").displayTime(), is("0\n0000\n0000\nYYRY0000000\nY000"));
+    }
+
 
 }
